fe977e1ed9f5c17e53d6e76f814d7bde5597b5cf
Yngve Svalestuen <yngve@outlook.com>
2016-01-27 12:40:27 +0100

    Added more auth-prompt tests for basic setup

--- src/components/auth-prompt.component.spec.ts
+++ src/components/auth-prompt.component.spec.ts
@@ -3,15 +3,45 @@ import {
     it,
     expect,
     injectAsync,
+    beforeEachProviders,
     TestComponentBuilder
 } from 'angular2/testing';
+
+import {provide} from 'angular2/core';
+
+import {LoggedOutService} from '../services/login-service.mock';
+import {LoginService} from '../services/login-service';
 
 import {AuthPromptComponent} from './auth-prompt.component';
 
 export function main() {
     describe('AuthPromptComponent', () => {
-        it('should be sane', () => {
-            expect(1 + 1).toBe(2);
-        });
+        beforeEachProviders(() => [
+            provide(LoginService, {useClass: LoggedOutService})
+        ]);
+        
+        it('should have a form', injectAsync([TestComponentBuilder], tcb => {
+            return tcb
+                .createAsync(AuthPromptComponent)
+                .then(fixture => {
+                    expect(fixture.debugElement.nativeElement.querySelector('form')).not.toBe(null);
+                });
+        }));        
+        
+        it('should have an input for the username', injectAsync([TestComponentBuilder], tcb => {
+            return tcb
+                .createAsync(AuthPromptComponent)
+                .then(fixture => {
+                    expect(fixture.debugElement.nativeElement.querySelector('form input')).not.toBe(null);
+                });
+        }));        
+        
+        it('should have a button for submitting', injectAsync([TestComponentBuilder], tcb => {
+            return tcb
+                .createAsync(AuthPromptComponent)
+                .then(fixture => {
+                    expect(fixture.debugElement.nativeElement.querySelector('form button')).not.toBe(null);
+                });
+        }));
     });
 }
